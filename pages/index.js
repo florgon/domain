c59005507656ec35894b2522c5592e5b6b400f17
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-06 00:48:28 +0400

    Migrate to NEXT blank setup.

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <Head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+
+      <meta name="author" CONTENT="https://kirillzhosul.site" />
+      
+      <meta name="description" content="Software solutions for everyone." />
+      <meta name="keywords" content="florgon, space, portal, services, kirillzhosul" />
+
+      <link rel="canonical" href="https://florgon.space" />
+      <link rel="alternate" hreflang="x-default" href="https://florgon.space" />
+      
+      <meta name="title" content="Florgon" />
+      <title>Florgon</title>
+    </Head>
+  )
+}
