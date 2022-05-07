ce2b8a221659f98a3a731c1c5ed54a300dc6ab5c
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-08 02:01:27 +0400

    Use lowercase pattern

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
 function App({ Component, pageProps }) {
   return (
