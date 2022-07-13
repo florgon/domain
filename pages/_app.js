45e9abfb0387385db376725cbfc54c02eb189c80
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-13 11:06:02 +0400

    Update favicon, logo192, Manifest.
    Close #15

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import Layout from '../components/layout'
+import Head from 'next/head';
+
 
 function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout title="Florgon">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="logo192.png" />
+        <link rel="manifest" href="manifest.json" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
